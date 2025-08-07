155fe236a54ae04af0c93e35974b716321329cde
Amit Kumar <amitk242122@gmail.com>
2025-08-07 22:54:12 +0530

    Added fourth_dsa.cpp

---
+++ fourth_dsa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+          int arr[]= {1,2,3,4,5,6,7,8};
+          int n=8;
+
+          for(int i=0; i<8; i+=2){
+                    cout<<arr[i]<<" ";
+          }
+          return 0;
+}
+
