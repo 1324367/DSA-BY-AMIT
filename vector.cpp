ab9fd1e08b887971985c6b19dac65d1c896e9617
Amit Kumar <amitk242122@gmail.com>
2025-08-12 21:56:45 +0530

    Added vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ int main(){
+          vector<int>pappu;
+          
+          pappu.push_back(10);
+
+          cout<<pappu[0];
+ 
+          return 0;
+
+ }
+ 
