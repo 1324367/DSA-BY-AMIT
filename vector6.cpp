f944368956b7995ae2ad19ae5c4f6f2d6369abe1
Amit Kumar <amitk242122@gmail.com>
2025-08-15 18:36:45 +0530

    Added vector6.cpp

---
+++ vector6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+          vector<char>v={'A','m','i','t'};
+
+          //traverse the code 
+          for(int i=0; i<v.size(); i++){
+                    cout<<v[i]<<" ";
+          }
+
+          return 0;
+}
